3aa09e7b9cecfce0ecbe015d5e284e5ee04136c2
Luis Costa Brochado <LuisCosta.Brochado@isobar.onmicrosoft.com>
2021-05-18 13:00:59 +0100

    Adding Mutation and repeating the same process

---
+++ GraphQLProject/Mutation/ProductMutation.cs
@@ -0,0 +1,43 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQLProject.Interfaces;
+using GraphQLProject.Model;
+using GraphQLProject.Type;
+
+namespace GraphQLProject.Mutation
+{
+    public class ProductMutation : ObjectGraphType
+    {
+        public ProductMutation(IProduct productService)
+        {
+            // Create product
+            Field<ProductType>("createProduct", arguments: new QueryArguments(new QueryArgument<ProductInputType> { Name = "product" }),
+                resolve: context =>
+                {
+                    return productService.AddProduct(context.GetArgument<Product>("product"));
+                });
+
+            // Update product
+            Field<ProductType>("updateProduct", 
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" },
+                    new QueryArgument<ProductInputType> { Name = "product" }),
+                resolve: context =>
+                {
+                    var productId = context.GetArgument<int>("id");
+                    var productObj = context.GetArgument<Product>("product");
+
+                    return productService.UpdateProduct(productId, productObj);
+                });
+
+            // Delete product
+            Field<StringGraphType>("deleteProduct",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+                resolve: context =>
+                {
+                    var productId = context.GetArgument<int>("id");
+                    productService.DeleteProduct(productId);
+                    return "The product with the product id " + productId + "was removed!";
+                });
+        }
+    }
+}
--- GraphQLProject/Schema/ProductSchema.cs
+++ GraphQLProject/Schema/ProductSchema.cs
@@ -1,4 +1,5 @@
-﻿using GraphQLProject.Query;
+﻿using GraphQLProject.Mutation;
+using GraphQLProject.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,10 @@ namespace GraphQLProject.Schema
 {
     public class ProductSchema : GraphQL.Types.Schema
     {
-        public ProductSchema(ProductQuery productQuery)
+        public ProductSchema(ProductQuery productQuery, ProductMutation productMutation)
         {
             Query = productQuery;
+            Mutation = productMutation;
         }
     }
 }
